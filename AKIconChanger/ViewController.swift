8fb94d89653b9db343f1ba898bc4781e3ffa0244
Ashish Kakkad <ashishkakkad8@gmail.com>
2020-03-28 17:42:06 +0530

    How to set alternate icon for an iOS app programmatically?

--- AKIconChanger/ViewController.swift
+++ AKIconChanger/ViewController.swift
@@ -15,6 +15,28 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func btnILikeTomPressed(_ sender: UIButton) {
+        changeIcon(iconName: "TomIcon")
+    }
 
+    @IBAction func btnILikeJerryPressed(_ sender: UIButton) {
+        changeIcon(iconName: "JerryIcon")
+    }
+
+    @IBAction func btnILikeBothPressed(_ sender: UIButton) {
+        UIApplication.shared.setAlternateIconName(nil)
+    }
+
+    func changeIcon(iconName: String) {
+        if UIApplication.shared.supportsAlternateIcons {
+            UIApplication.shared.setAlternateIconName(iconName){ error in
+                if let error = error {
+                    print(error.localizedDescription)
+                } else {
+                    print("Done!")
+                }
+            }
+        }
+    }
 }
 
